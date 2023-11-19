bd8bf37bf42288a96633bd4b0d355ff297b2953c
mahat sumit <mahatsumit5@gmail.com>
2023-11-20 00:12:42 +1100

    .

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 export const serverUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "https://messenger-ezhi.onrender.com";
+    : "https://messenger-j2bf.onrender.com";
