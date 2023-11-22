513eed5f04f5a93a1efb075aa180f6659c8e2af2
Sumit Mahat <mahatsumit5@gmail.com>
2023-11-22 23:37:19 +1100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="*" element={<Home />} />
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
